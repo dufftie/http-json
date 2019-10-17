955dd75b6905deadd1f07283b5067967df9664ac
Opilane TTHK <opilane@tthk.ee>
2019-10-17 11:36:33 +0300

    I vina

--- app.js
+++ app.js
@@ -1,4 +1,19 @@
 const http = new myHTTP;
-const user = http.get('https://jsonplaceholder.typicode.com/users')
+/*const user = http.get('https://jsonplaceholder.typicode.com/users')
             .then(data => console.log("DATA Recieved: ", data))
-            .catch(error => console.log(error));
+            .catch(error => console.log(error));*/
+
+const url = 'https://jsonplaceholder.typicode.com/users';
+const data = {
+    name: 'John Doe',
+    username: 'johndoe',
+    email: 'jdoe@gmail.com'
+}
+
+/*http.post(url, data)
+.then(data => console.log(data))
+.catch(error => console.log(error));*/
+
+http.put('https://jsonplaceholder.typicode.com/users/2', data)
+.then(data => console.log(data))
+.catch(error => console.log(error));
